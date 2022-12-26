407bbf7b109b42dcb81038d0f9ce7e13ec921163
mel-mouk <mathilde@elmouktafi.fr>
2022-12-26 12:44:55 +0100

    Day 1 - Calorie Counting

---
+++ day-1/day-1-part-1.js
@@ -0,0 +1,38 @@
+// https://adventofcode.com/2022/day/1
+
+const getInput = () => {
+	return new Promise((resolve, reject) => {
+		const stdin = process.openStdin();
+		let data = "";
+
+		stdin.on('data', function(chunk) {
+			data += chunk;
+		});
+
+		stdin.on('end', function() {
+			resolve(data);
+		});
+	});
+};
+
+(async () => {
+	const input = await getInput();
+
+	const result = solve(input);
+
+	console.log(result);
+})();
+
+const parseInput = (input) => {
+	return input.split("\n\n").map(list => {
+		return list.split("\n").filter(line => line.length);
+	});
+};
+
+const solve = (input) => {
+	const elvesItems = parseInput(input);
+
+	const elvesCalories = elvesItems.map(list => list.reduce((acc, cur) => acc + +cur, 0));
+
+	return elvesCalories.sort((a, b) => b - a)[0];
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+# My solutions for the advent of code 2022
+https://adventofcode.com/2022
+
+## How to run a solution ?
+- Go into the required folder (day-X) and create your input file. (ex: input-challenge.txt with the input content inside)
+- Cat the file's content and pipe it into the script
+
+Example :
+
+```
+cd day-1
+cat input-challenge.txt | node ex1.1.js
+```
